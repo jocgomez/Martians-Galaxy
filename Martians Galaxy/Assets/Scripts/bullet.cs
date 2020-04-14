12b97fe7a57f632ae8deec562757595c1144be67
jocgomez <jose_dav.gomez@uao.edu.co>
2020-04-14 16:14:31 -0500

    Shoot

---
+++ Martians Galaxy/Assets/Scripts/bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+    public float speed = 20f;
+    public int damage = 40;
+    public Rigidbody2D rb;
+	
+	// Update is called once per frame
+	void Update () {
+
+        rb.velocity = transform.right * speed;
+
+	}
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
+
